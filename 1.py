1c6b6b0e1b88c45c3fbf950b05b19e54b713404a
nukala-srikar2002 <61324000+nukala-srikar2002@users.noreply.github.com>
2022-11-21 15:41:12 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,2 +1,10 @@
-print("hwllo")
-
+n = int(input())
+if n == 0:
+    print(0)
+a = []
+while(n):
+    rem = n%3
+    n = n//3
+    a.append(str(rem))
+ternary = int("".join(reversed(a)))
+print(ternary)
